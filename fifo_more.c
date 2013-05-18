fbf6664fdba5072c028207104a14793121906490
weizmann <zhengwei0828@126.com>
2013-05-19 00:02:51 +0800

    add new file fifo_more.c for fifo testings

---
+++ fifo_more.c
@@ -0,0 +1,30 @@
+/*
+ * fifo_mre.c
+ *
+ *  Created on: 2013-5-18
+ *      Author: zhengwei
+ */
+#include "apue.h"
+
+#define DEF_PAGE "/usr/bin/more"
+/*
+ * execute more : /usr/bin/more
+ * */
+
+int main(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
